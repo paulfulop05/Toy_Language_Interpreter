84b1082e2614a42e099521cc881c6f3896ac3902
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2025-12-10 16:03:20 +0200

    optimising heap class even more -> should inherit from mymap

--- src/model/states/MyHeap.java
+++ src/model/states/MyHeap.java
@@ -16,15 +16,6 @@ public class MyHeap extends MyMap<IntValue, Value> {
         this.freeLocation = 1;
     }
 
-    @Override
-    public Value lookup(IntValue key) throws UndefinedIdException, ValueNotFoundException {
-        if (!isDefined(key)) throw new UndefinedIdException(String.valueOf(key));
-        var value = map.get(key);
-
-        if (value == null) throw new ValueNotFoundException();
-        return value;
-    }
-
     public int add(Value value) { // returns the address in which the value was added
         super.add(new IntValue(freeLocation), value);
         int oldFreeLocation = freeLocation;
@@ -41,15 +32,5 @@ public class MyHeap extends MyMap<IntValue, Value> {
             }
         }
         freeLocation = i;
-    }
-
-    @Override
-    public String toString(){
-        String text = "";
-        for (var key  : map.keySet()) {
-            text += key.val() + " --> " + map.get(key).toString() + '\n';
-        }
-
-        return text;
     }
 }
