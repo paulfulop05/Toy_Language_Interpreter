47869de2a3397444b1f7aa4c1d70821f6759735b
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2026-02-01 19:23:13 +0200

    making cyclic barrier methods thread-safe

--- src/model/states/map/BarrierTable.java
+++ src/model/states/map/BarrierTable.java
@@ -1,7 +1,26 @@
 package model.states.map;
 
+import exceptions.UndefinedIdException;
+import exceptions.ValueNotFoundException;
 import javafx.util.Pair;
 
 import java.util.List;
 
-public class BarrierTable extends MyHeap<Pair<Integer, List<Integer>>> { }
+public class BarrierTable extends MyHeap<Pair<Integer, List<Integer>>> {
+
+    public synchronized Pair<Integer, List<Integer>> lookup(Integer key) throws UndefinedIdException, ValueNotFoundException {
+        return super.lookup(key);
+    }
+
+    public synchronized void update(Integer key, Pair<Integer, List<Integer>> value) throws UndefinedIdException {
+        super.update(key, value);
+    }
+
+    public synchronized int add(Pair<Integer, List<Integer>> value) {
+        return super.add(value);
+    }
+
+    protected synchronized void updateFreeLocation() {
+        super.updateFreeLocation();
+    }
+}
